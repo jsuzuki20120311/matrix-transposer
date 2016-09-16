667c8663a77baecdba3e78c5ea2fdfc603cacb67
Jun <shukatu.2012.5.25@gmail.com>
2016-09-16 15:58:57 +0900

    bugfix.

--- src/main.js
+++ src/main.js
@@ -6,34 +6,49 @@
 
 class MatrixTransposer {
 
+  static get DEFAULT_OPTION() {
+    return {
+      referenceIndex: 0,
+      defaultValue: null
+    };
+  }
+
   /**
    * 引数で与えられた二次元配列の行と列を入れ替えた配列を新たに生成し戻します。
    *
    * @param twoDimensionalArray {Array}
+   * ＠param option {Object}
    * @returns {Array}
    */
-  static arrayTranspose(twoDimensionalArray) {
-
+  static arrayTranspose(twoDimensionalArray, option) {
     if (!Array.isArray(twoDimensionalArray)) {
-      throw new Error('Illegal argument. Argument is not Array.');
+      throw new Error('Illegal argument. First argument is not array.');
+    }
+    if (option && typeof option !== 'object') {
+      throw new Error('Illegal argument. Second argument is not object.');
+    }
+    if (option && (option.referenceIndex || option.referenceIndex === 0)
+      && typeof option.referenceIndex !== 'number'
+    ) {
+      throw new Error(`Illegal argument. Second argument[referenceIndex] is not number.`);
+    }
+    let targetIndex = option.referenceIndex || MatrixTransposer.DEFAULT_OPTION.referenceIndex;
+    if (!Array.isArray(twoDimensionalArray[targetIndex])) {
+      throw new Error(`Illegal argument. First Argument[${targetIndex}] is not Array.`);
     }
 
-    if (twoDimensionalArray.length === 0) {
-      return [];
-    }
-
-    let results = [];
-    for (let i = 0; i < twoDimensionalArray[0].length; i++) {
-      let result = [];
-      for (let j = 0; j < twoDimensionalArray.length; j++) {
-        if (!Array.isArray(twoDimensionalArray[j])) {
-          throw new Error(`Illegal argument. Argument[${j}] is not Array.`);
+    return twoDimensionalArray[targetIndex].map((value, columnIndex) => {
+      return twoDimensionalArray.map((row, rowIndex) => {
+        if (!Array.isArray(row)) {
+          throw new Error(`Illegal argument. Argument[${rowIndex}] is not Array.`);
         }
-        result.push(twoDimensionalArray[j][i]);
-      }
-      results.push(result);
-    }
-    return results;
+        if (typeof row[columnIndex] === 'undefined') {
+          return (option && (option.defaultValue || option.defaultValue === 0)) ? option.defaultValue : MatrixTransposer.DEFAULT_OPTION.defaultValue;
+        } else {
+          return row[columnIndex];
+        }
+      });
+    });
   }
 
 }
